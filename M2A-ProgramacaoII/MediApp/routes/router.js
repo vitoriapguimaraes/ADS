d6d4a4734f5d76f4055aa070175bd8db08284158
vitoriapguimaraes <vpistoriguimaraes@gmail.com>
2025-08-24 11:37:14 -0300

    class5 update

--- M2A-ProgramacaoII/MediApp/routes/router.js
+++ M2A-ProgramacaoII/MediApp/routes/router.js
@@ -1,4 +1,8 @@
 import { express } from "express";
+import appointmentController from "./AppointmentController";
+import doctorController from "./DoctorController";
+import pacientController from "./PacientController";
+import prescriptionController from "./PrescriptionController";
 
 let router = express.Router();
 
@@ -6,5 +10,10 @@ router.get("/", function (req, res) {
   console.log("hi");
   res.status(200).json({ message: "hi!" });
 });
+
+router.use("/", appointmentController);
+router.use("/", doctorController);
+router.use("/", pacientController);
+router.use("/", prescriptionController);
 
 export default router;
