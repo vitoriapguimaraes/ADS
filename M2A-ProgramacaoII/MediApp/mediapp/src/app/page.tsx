b492fc0e2353c1c6698113a85c56c135317d24a8
vitoriapguimaraes <vpistoriguimaraes@gmail.com>
2025-09-18 19:13:43 -0300

    end programacao II

--- M2A-ProgramacaoII/MediApp/mediapp/src/app/page.tsx
+++ M2A-ProgramacaoII/MediApp/mediapp/src/app/page.tsx
@@ -1,103 +1,85 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const router = useRouter();
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const authentication = async (e: any) => {
+    e.preventDefault();
+    setError(null);
+
+    if (login != "" && password != "") {
+      const formData = {
+        login: login,
+        password: password,
+      };
+
+      const add = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const content = await add.json();
+      if (content.token) {
+        sessionStorage.setItem("token", content.token);
+        router.push("/home");
+      } else {
+        setError(content.error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex-col items-center p-7 rounded-2xl">
+        <form className="w-full" onSubmit={authentication}>
+          <span className="font-bold text-blue-500 py-2 block text-2xl">
+            Login
+          </span>
+          <div className="w-full py-2">
+            <label htmlFor="" className="text-sm font-bold py-2 block">
+              Usuário
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
+              onChange={(e: any) => setLogin(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          </div>
+          <div className="w-full py-2">
+            <label htmlFor="" className="text-sm font-bold py-2 block">
+              Senha
+            </label>
+            <input
+              name="login"
+              type="password"
+              className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
+              onChange={(e: any) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="w-full py-2">
+            <button className="w-20 p-2 text-white border-gray-200 border-[1px] rounded-sm bg-green-400">
+              Login
+            </button>
+          </div>
+          <div>
+            {error && (
+              <div
+                className="p-2 text-white border-gray-200 border-[1px] rounded-sm bg-red-400"
+                style={{ color: "white" }}
+              >
+                {error}
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
